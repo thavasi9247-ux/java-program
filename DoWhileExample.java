ea1f6371304571110bb1ffd70398bc52c655c030
thavasi9247-ux <thavasi9247@mountzion.ac.in>
2025-08-05 12:35:36 +0530

    Add files via upload

---
+++ DoWhileExample.java
@@ -0,0 +1,10 @@
+public class DoWhileExample {
+    public static void main(String[] args) {
+        int i = 1;
+
+        do {
+            System.out.println("Count is: " + i);
+            i++;
+        } while (i <= 5);
+    }
+}
